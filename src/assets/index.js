bf919d85630487024f38f71273cdf413d9e1713a
Marwan <marwanhisham5@gmail.com>
2023-07-26 00:35:14 +0200

    content section edited

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,14 @@
-import logo from "./logo.png";
+import logo from './logo.png'
+import contact from './communicate.png'
+import email from './email.png'
+import revision from './revision.png'
+import launch from './launch.png'
 
-export {
-    logo
-};
+
+export{
+    logo,
+    contact,
+    email,
+    revision,
+    launch
+}
