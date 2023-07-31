0de74a1c769031d6c132b85d099a56b26be64a80
Marwan <marwanhisham5@gmail.com>
2023-07-31 06:04:03 +0200

    minor

--- src/components/feature.tsx
+++ src/components/feature.tsx
@@ -25,7 +25,7 @@ const features = [
 const Feature = () => {
   
   return (
-    <div className="overflow-hidden bg-[#f4eeff] py-10 sm:py-14">
+    <div className="overflow-hidden bg-[#f7f5fe] py-10 sm:py-14">
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
       <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
         <div className="lg:pr-8 lg:pt-4">
